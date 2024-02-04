ce03056dc181526ee5e8b302342d0ba866342369
mt <mt@example.com>
2024-02-04 21:42:49 +0200

    node-js2

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ mongoose.connect("mongodb+srv://mm1632002:1632002@mohamed1.lk0lj1s.mongodb.net/?
 
 //--------------------------
 //-----app---------
-const app=express();
-app.use(express.json())
+// const app=express();
+// app.use(express.json())
 
-app.get("/mt",function(req,res){
+// app.get("/mt",function(req,res){
     
-  console.log(req.query); //{ g: '50' }
+//   console.log(req.query); //{ g: '50' }
      
 
 
 // res.send(` iam is ${n1}`)
-})
+// })
 //----------------------------
 // body بيراميتر
 // app.get("/mt2",function(req,res){
@@ -40,17 +40,17 @@ app.get("/mt",function(req,res){
 // res.send(`the total is ${t}`)
 // })
 //----------------------------------
-app.get("/n",function(req,res){
-    let n="";
-    for(let i=0;i<=100;i++){
-n+=i +"-"
-    }
-res.send("<h1>tarek</h1>")
-res.sendFile(__dirname +"/views/index.html")// ملف html
-res.render("index.ejs",{
-    name:"n",
-    number:n,
-})
+// app.get("/n",function(req,res){
+//     let n="";
+//     for(let i=0;i<=100;i++){
+// n+=i +"-"
+//     }
+// res.send("<h1>tarek</h1>")
+// res.sendFile(__dirname +"/views/index.html")// ملف html
+// res.render("index.ejs",{
+//     name:"n",
+//     number:n,
+// })
 
 //------------------------------------
 // res.json({
@@ -58,7 +58,7 @@ res.render("index.ejs",{
 //     age:req.query.age,
 //     mm:true
 // })
-})
+// })
 
 // app.get("/hi",(req,res)=>{
 // res.send("hhhhhhhhhhhhhhh")
@@ -76,55 +76,56 @@ res.render("index.ejs",{
 //-----ARTICLES ENOPOINTS--- تحميل البيانات في قاعد البيانات
 //post الافضل
 // ------------1-----------------
-app.post("/s1",async(req,res)=>{
+// app.post("/s1",async(req,res)=>{
     
-    let newArticle=new Article();
-    const t=req.body.title 
-    const b=req.body.body
-    const m=req.body.mobel
+//     let newArticle=new Article();
+//     const t=req.body.title 
+//     const b=req.body.body
+//     const m=req.body.mobel
 
-    newArticle.title=t;
-    newArticle.body=b;
-    newArticle.mobel=m;
+//     newArticle.title=t;
+//     newArticle.body=b;
+//     newArticle.mobel=m;
 
-    newArticle.numberm=m; //11245663 شال الصفر علشان ركم
-    await newArticle.save();
-    res.send("save")
-}) 
+//     newArticle.numberm=m; //11245663 شال الصفر علشان ركم
+//     await newArticle.save();
+//     res.send("save")
+// }) 
+
 
 // ------------2-----------------
-app.post("/s2",async(req,res)=>{
+// app.post("/s2",async(req,res)=>{
     
-    let newArticle=new Article();
-    const t="mmmm" 
-    const b="tttttt"
-    const m="nnnnnnnnnnnnnnnn"
+//     let newArticle=new Article();
+//     const t="mmmm" 
+//     const b="tttttt"
+//     const m="nnnnnnnnnnnnnnnn"
 
-    newArticle.title=t;
-    newArticle.body=b;
-    newArticle.mobel=m;
+//     newArticle.title=t;
+//     newArticle.body=b;
+//     newArticle.mobel=m;
 
-    newArticle.numberm=m; //11245663 شال الصفر علشان ركم
-    await newArticle.save();
-    res.send("save")
-}) 
+//     newArticle.numberm=m; //11245663 شال الصفر علشان ركم
+//     await newArticle.save();
+//     res.send("save")
+// }) 
 
 // -------------------------------------------
 // اطبع قاعدالبيانات كلهfind--------1-----
 
-app.get("/cc",async(req,res)=>{
-    const f= await Article.find();
-    // find طباعه
-    res.json(f)
-})
+// app.get("/cc",async(req,res)=>{
+//     const f= await Article.find();
+//     // find طباعه
+//     res.json(f)
+// })
 
 // اطبع id معين من قاعد البيانات findById-------2------
-app.get("/c1/:cid",async(req,res)=>{
-    const id =req.params.cid
-    const idd= await Article.findById(id);
-    // findById id معين
-    res.json(idd)
-})
+// app.get("/c1/:cid",async(req,res)=>{
+//     const id =req.params.cid
+//     const idd= await Article.findById(id);
+//     // findById id معين
+//     res.json(idd)
+// })
 // show html------------3-----------
 
 app.get("/showArticle",async(req,res)=>{
@@ -132,15 +133,17 @@ app.get("/showArticle",async(req,res)=>{
     res.render("showArticle.ejs",{
         aaS:s
     })
+
+
     })
 // --------------------------------------------------------
  // findByIdAndDelete حذف id معين
-app.delete("/c2/:cid",async(req,res)=>{
-    const id =req.params.cid
-    const idd= await Article.findByIdAndDelete(id);
-    // findByIdAndDelete حذف id معين
-    res.json(idd)
-})
+// app.delete("/c2/:cid",async(req,res)=>{
+//     const id =req.params.cid
+//     const idd= await Article.findByIdAndDelete(id);
+//     // findByIdAndDelete حذف id معين
+//     res.json(idd)
+// })
 
 
 app.listen(300,()=>{
